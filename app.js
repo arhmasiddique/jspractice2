36d3e54a8be7ebdf6f4c619f349ecbea605b3b27
ARHAMA SIDDIQUE <138670043+arhmasiddique@users.noreply.github.com>
2023-10-14 08:33:19 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+//PRACTICE EXERCISE 1
+
+console.log("ARHAMA");
+
+console.log("HELLO WORLD");
+
+
+
+// let a = 100;
+console.log(" here we write the value of a which is equal to 100")
+
+
+
+//Making an js file with the name of app.js
+//link app.js to html file with the help of script tag.
+
+
+
+
+//SINGLE LINE COMMENT WITH  "//"
+
+//1. You do not want to execute a piece of code while running the script, so you
+
+//MULTIPLE LINES COMMENT WITH  OPEN -->  "/*"   -->"*/"
+
+/*comment it out so it gets ignored by the interpreter.
+2. Metadata. Adding some context to the code, such as the author, and a
+description of what the file covers.
+3. Adding comments to specific parts of the code to explain what is happening
+or why a certain choice has been made.*/
---
+++ app2.js
@@ -0,0 +1,87 @@
+
+//EXERCISE:2.1
+
+let str1 = 'Laurence'; 
+
+console.log(str1, typeof str1);
+
+let str2 = "Svekis";   
+
+console.log(str2, typeof str2);
+
+let val1 = undefined;  
+
+console.log(val1, typeof val1);
+
+let val2 = null;      
+
+console.log(val2, typeof val2);
+
+let myNum = 1000;      
+
+console.log(myNum, typeof myNum);
+
+//EXERCISE:2.2
+
+let hello= "HELLO,"
+let myname ="I AM ARHAMA"
+let myage =" I AM 23 year old"
+let code="i can code JAVASCRIPT "
+let bool=true;
+
+console.log(hello,myname,myage,code,bool)
+
+//EXERCISE:2.3
+
+
+
+let nr1 = prompt("enter value of A");
+let nr2 = 2;
+let result1 = nr1 ** nr2;
+//console.log(result1);
+let nr3 = prompt("enter value of b");
+let nr4 = 2;
+let result2 = nr3 ** nr4;
+//console.log(result2);
+document.write (result1 + result2 );
+
+
+//EXERCISE:2.4
+
+let a = 5;
+let b = 10;
+let c = 3;
+
+console.log(b+a);
+console.log(c/a);
+console.log (c%b);
+
+//project
+
+
+//Miles-to-kilometers converter:
+let miles=(100)
+let kilometersdistance = miles*1.60934;
+
+console.log(kilometersdistance)
+
+
+//BMI CALCULATOR
+
+let heightintoinches=(900)
+let weightinpounds=(900)
+//convert
+let centemeter = heightintoinches*2.54;
+let pounds = weightinpounds*2.2046;
+//print
+console.log(centemeter)
+console.log(pounds)
+
+//calculate
+let result=(pounds/centemeter)
+console.log(result)
+
+
+
+
+
